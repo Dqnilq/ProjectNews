14fb13d2d8130656c655fcf9844e88d1e3a507a5
Danila Makarov <makarov01V@ya.ru>
2020-12-15 22:00:40 +0300

    comments front

---
+++ Models/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bussines.Models
+{
+    public class Comment : IEntity 
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public int UsersId { get; set; }
+        public string UsersName { get; set; }
+        public string CommentText { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Comment(int usersId, string commentText, DateTime creationDate)
+        {
+            UsersId = usersId;
+            CommentText = commentText;
+            CreationDate = creationDate;
+        }
+
+        public Comment(int courseId, int usersId, string commentText, DateTime creationDate)
+        {
+            CourseId = courseId;
+            UsersId = usersId;
+            CommentText = commentText;
+            CreationDate = creationDate;
+        }
+    }
+}
---
+++ Pages/BlogContent.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Bussines.Pages
+{
+    public class BlogContentModel : PageModel
+    {
+        private readonly ILogger<BlogContentModel> _logger;
+
+        public BlogContentModel(ILogger<BlogContentModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
